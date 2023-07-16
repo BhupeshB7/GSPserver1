de8e264b9cf16a3c9d1a70c4b80f088a9fdea7d5
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-07-16 16:59:29 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -248,12 +248,86 @@ router.get("/profile", auth, async (req, res) => {
 });
 
 
-router.post('/profileUpdate', async (req, res) => {
-  const {userId}= req.params;
+// router.post('/profileUpdate', async (req, res) => {
+//   const {userId}= req.params;
+
+//   try {
+//     // const user = await User.findById(req.user.id);
+//     const user = await User.findOne({userId : userId});
+
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+
+//     if (req.body.name) {
+//       user.name = req.body.name.trim();
+//     }
+//     if (req.body.accountHolderName) {
+//       user.accountHolderName = req.body.accountHolderName.trim();
+//     }
+
+//     if (req.body.bio) {
+//       user.bio = req.body.bio.trim();
+//     }
+//     if (req.body.address) {
+//       user.address = req.body.address.trim();
+//     }
+//     if (req.body.accountNo) {
+//       user.accountNo = req.body.accountNo.trim();
+//     }
+//     if (req.body.ifscCode) {
+//       user.ifscCode = req.body.ifscCode.trim();
+//     }
+//      //For google Pay
+//      if (req.body.GPay) {
+//       const GPay = req.body.GPay.trim();
+//       const GPayExists = await User.findOne({ GPay });
+//       if (GPayExists && GPayExists._id.toString() !== user._id.toString()) {
+//         return res.status(400).json({ error: 'Gpay number already exists' });
+//       }
+//       user.GPay = GPay;
+//     }
+//     if (req.body.mobile) {
+//       const mobile = req.body.mobile.trim();
+//       const mobileExists = await User.findOne({ mobile });
+//       if (mobileExists && mobileExists._id.toString() !== user._id.toString()) {
+//         return res.status(400).json({ error: 'Mobile number already exists' });
+//       }
+//       user.mobile = mobile;
+//     }
+// if (req.body.aadhar) {
+//       const aadhar = req.body.aadhar.trim();
+//       const aadharExists = await User.findOne({ aadhar });
+//       if (aadharExists && aadharExists._id.toString() !== user._id.toString()) {
+//         return res.status(400).json({ error: 'Aadhar number already exists' });
+//       }
+//       user.aadhar = aadhar;
+//     }
+//     if (req.body.email) {
+//       const email = req.body.email.trim().toLowerCase();
+//       const emailExists = await User.findOne({ email });
+//       if (emailExists && emailExists._id.toString() !== user._id.toString()) {
+//         return res.status(400).json({ error: 'Email already exists' });
+//       }
+//       user.email = email;
+//     }
+
+//     await user.save();
+
+//     res.json({ message: 'Profile updated successfully' });
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send('Server Error for updation');
+//   }
+// });
+
+
+router.post('/profileUpdate',auth, async (req, res) => {
+  // const {id}= req.params;
 
   try {
-    // const user = await User.findById(req.user.id);
-    const user = await User.findOne({userId : userId});
+    const user = await User.findById(req.user.id);
+    // const user = await User.findById({_id : id});
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
@@ -320,9 +394,6 @@ if (req.body.aadhar) {
     res.status(500).send('Server Error for updation');
   }
 });
-
-
-
 
 router.post('/activeuser/:userId', async (req, res) => {
   const { userId } = req.params;
