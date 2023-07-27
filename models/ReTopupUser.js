5df312f685dd00a937cb20e08f6a9aeb477349a3
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-07-27 09:32:53 +0530

    update

--- models/ReTopupUser.js
+++ models/ReTopupUser.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     userID: { type: String, required: true },
-    balance: { type: Number, required: true },
-    income: { type: Number, required: true },
-    selfIncome: { type: Number, required: true },
-    LevelIncome: { type: Number, required: true },
+    oldWallet: { type: Number, required: true },
+    reservedWallet: { type: Number, required: true },
   });
   module.exports = mongoose.model('RetopUpUser', userSchema);
