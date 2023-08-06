e786ebebafc89b3439fac651d1d7e1e6c2d1ee7e
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-08-06 17:50:22 +0530

    update

--- routes/withdraw.js
+++ routes/withdraw.js
@@ -6,65 +6,117 @@ const WithdrawalReq = require('../models/WithdrawReq');
 // router.use(express.json());
 const router = express.Router();
 
+// All withdrawal Code
+// router.post('/withdraw/:userId', async (req, res) => {
+//   const { userId} = req.params;
+//   const { amount, GPay, ifscCode, accountNo, accountHolderName } = req.body;
+//   const user = await User.findOne({ userId: userId});
+
+//   // Check if the withdrawal amount is greater than 0
+//   if (amount <= 0) {
+//     return res.status(400).json({ error: 'Withdrawal amount should be greater than 0' });
+//   }
+//   // Check if the withdrawal amount is greater than or equal to 500
+//   if (amount >= 500) {
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+//     // Check if the user is active
+//   // if (!user.is_active) {
+//   //   return res.status(400).json({ error: 'User is not active and cannot make withdrawals' });
+//   // }
+//     //   // check if the withdrawal amount is greater than or equal to 500
+//       if (amount < 500) {
+//         return res.status(400).json({ error: 'Minimum withdrawal amount is 500 Rs' });
+//       }
+//     // Check if user has at least two direct referrals
+//     const count = await User.countDocuments({ sponsorId: userId, is_active:true   });
+//     // console.log(count)
+//     if (count < 2) {
+//       return res.status(400).json({ error: 'Minimum Two Direct for Withdrawal' });
+//     }
+
+//     // Check if user balance is sufficient for the withdrawal
+//     if (user.balance < amount) {
+//       return res.status(400).json({ error: 'Insufficient balance' });
+//     }
+
+//     // Create a new withdrawal request
+//     const withdrawalRequest = new WithdrawalReq({
+//       userId,
+//       amount,
+//       GPay,
+//       ifscCode,
+//       accountNo,
+//       accountHolderName
+//     });
+
+//     await withdrawalRequest.save();
+
+//     // Update user withdrawal and balance
+//     user.withdrawal += amount;
+//     user.balance -= amount;
+//     await user.save();
+
+//     return res.json({ success: true });
+//   } else if (amount === 200) {
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+
+//     // Check if user has already made a withdrawal of 200 Rs
+//     if (user.withdrawalDone && amount === 200) {
+//       return res.status(403).json({ error: 'Withdrawal of 200 Rs already done' });
+//     }
+
+//     // Check if user balance is sufficient for the withdrawal
+//     if (user.balance < amount) {
+//       return res.status(400).json({ error: 'Insufficient balance' });
+//     }
+
+//     // Create a new withdrawal request
+//     const withdrawalRequest = new WithdrawalReq({
+//       userId,
+//       amount,
+//       GPay,
+//       ifscCode,
+//       accountNo,
+//       accountHolderName
+//     });
+
+//     await withdrawalRequest.save();
+
+//     // Update user withdrawal and balance
+//     user.withdrawal += amount;
+//     user.balance -= amount;
+//     user.withdrawalDone = true;
+//     await user.save();
+
+//     return res.json({ success: true });
+//   } else {
+//     return res.status(400).json({ error: 'Minimum withdrawal amount is 500 Rs' });
+//   }
+// });
+// Withdrawal code ENd
+
 router.post('/withdraw/:userId', async (req, res) => {
-  const { userId} = req.params;
+  const { userId } = req.params;
   const { amount, GPay, ifscCode, accountNo, accountHolderName } = req.body;
-  const user = await User.findOne({ userId: userId});
+  const user = await User.findOne({ userId: userId });
 
   // Check if the withdrawal amount is greater than 0
   if (amount <= 0) {
     return res.status(400).json({ error: 'Withdrawal amount should be greater than 0' });
   }
-  // Check if the withdrawal amount is greater than or equal to 500
-  if (amount >= 500) {
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    // Check if the user is active
-  // if (!user.is_active) {
-  //   return res.status(400).json({ error: 'User is not active and cannot make withdrawals' });
-  // }
-    //   // check if the withdrawal amount is greater than or equal to 500
-      if (amount < 500) {
-        return res.status(400).json({ error: 'Minimum withdrawal amount is 500 Rs' });
-      }
-    // Check if user has at least two direct referrals
-    const count = await User.countDocuments({ sponsorId: userId, is_active:true   });
-    // console.log(count)
-    if (count < 2) {
-      return res.status(400).json({ error: 'Minimum Two Direct for Withdrawal' });
-    }
 
-    // Check if user balance is sufficient for the withdrawal
-    if (user.balance < amount) {
-      return res.status(400).json({ error: 'Insufficient balance' });
-    }
+  // Check if the user is active
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
 
-    // Create a new withdrawal request
-    const withdrawalRequest = new WithdrawalReq({
-      userId,
-      amount,
-      GPay,
-      ifscCode,
-      accountNo,
-      accountHolderName
-    });
-
-    await withdrawalRequest.save();
-
-    // Update user withdrawal and balance
-    user.withdrawal += amount;
-    user.balance -= amount;
-    await user.save();
-
-    return res.json({ success: true });
-  } else if (amount === 200) {
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
+  if (amount === 200) {
     // Check if user has already made a withdrawal of 200 Rs
-    if (user.withdrawalDone && amount === 200) {
+    if (user.withdrawalDone) {
       return res.status(403).json({ error: 'Withdrawal of 200 Rs already done' });
     }
 
@@ -80,7 +132,7 @@ router.post('/withdraw/:userId', async (req, res) => {
       GPay,
       ifscCode,
       accountNo,
-      accountHolderName
+      accountHolderName,
     });
 
     await withdrawalRequest.save();
@@ -93,7 +145,7 @@ router.post('/withdraw/:userId', async (req, res) => {
 
     return res.json({ success: true });
   } else {
-    return res.status(400).json({ error: 'Minimum withdrawal amount is 500 Rs' });
+    return res.status(400).json({ error: 'Only 200 Rs withdrawal allowed' });
   }
 });
 
